849d6aada2ecc9ffeb9c7f54f6c145b1e3c325a6
zamelkev <kevin.zamora.amela@gmail.com>
2022-10-27 18:54:40 +0200

    Creación de Clases / Métodos Getter & Setter y Asociación unidireccional Productos -> Fabricante

---
+++ net_cp1_Kevin-Zamora/Manufacturer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_cp1_Kevin_Zamora;
+public class Manufacturer {
+
+    int Id { get; set; }
+    string Nombre { get; set; }
+
+    List<Product> products { get; set; }
+
+    public Manufacturer() { }
+
+}
---
+++ net_cp1_Kevin-Zamora/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_cp1_Kevin_Zamora;
+
+public class Product {
+
+    int Id { get; set; }
+    string Nombre { get; set; }
+    int Peso { get; set; }
+    float Precio { get; set; }
+    int Cantidad { get; set; }
+    float Coste { get; set; }
+    DateTime FechaCreacion { get; set; }
+    Manufacturer Fabricante { get; set; }
+
+
+    public override string ToString() {
+        return $"Producto fabricado por {Fabricante} con: Id {Id} y Nombre {Nombre} y con un Peso de {Peso}. Actualmente hay en 'stock' {Cantidad} unidades. La fecha de creación fue: {FechaCreacion}";
+    }
+
+
+
+}
